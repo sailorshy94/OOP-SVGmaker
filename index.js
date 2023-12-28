c836f4243df1d0dd1bc22fe49153083e9577bd90
sailorshy94 <sbutle94@gmail.com>
2023-12-28 13:45:46 -0600

    added commentary to index.js file

---
+++ index.js
@@ -0,0 +1,15 @@
+// imports inquirer module
+const inquirer = require("inquirer");
+
+// will need to send info to shapes JS file
+
+// TODO: create prompt module to ask questions about SVG
+    // input = 3 or less characters;
+    // input text color choice = keyword or hexadecimal #);
+    // select choice (rawlist) for shape: circle, triangle, square;
+    // input shape color choice = keyword or hexadecimal #);
+
+// TODO: write function to create SVG file called 'logo.svg'
+    // output text in CLI = "Generated logo.svg" after
+    // then 300x200 pixel img that matches choices is shown
+
--- library/shapes.js
+++ library/shapes.js
@@ -1,3 +1,15 @@
 // TODO: create classes for Triangle, Circle and Square
 
 // common functions and properties can do in Shape parent class (use inheritance to reuse code in children)
+
+// Triangle Class Constructor
+
+// --------------------------------------------------------------------------------------------
+
+// Circle Class Constructor
+
+// --------------------------------------------------------------------------------------------
+
+// Square Class Constructor
+
+// --------------------------------------------------------------------------------------------
