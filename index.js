cd908c752e156413b11fc7350ee0696b22f8f5de
sailorshy94 <sbutle94@gmail.com>
2024-01-04 17:30:18 -0500

    added init function to index.js file to run prompt module, added note to import shapes.js file, added note to export shapes

--- index.js
+++ index.js
@@ -1,26 +1,56 @@
 // imports inquirer module
 const inquirer = require("inquirer");
 
-// will need to send info to shapes JS file
+// TODO: import shapes.js
+// const shapes = require("./library/shapes");
 
-// TODO: create prompt module to ask questions about SVG
+// TODO: set up an initializer function
+
+function init() {
+
+    // TODO: create prompt module to ask questions about SVG
     // input = 3 or less characters;
     // input text color choice = keyword or hexadecimal #);
     // select choice (rawlist) for shape: circle, triangle, square;
     // input shape color choice = keyword or hexadecimal #);
 
-// const prompt = inquirer.createPromptModule();
+    const questions = inquirer.createPromptModule();
 
-// prompt(questions).then();
+    // prompt(questions).then();
+    questions([
+        {
+            type: "input",
+            message:"",
+            name: "",
+            validate:,
+        },
+        {
+            type: "input",
+            message:"",
+            name: "", 
+        },
+        {
+            type: "rawlist",
+            message:"",
+            name: "",
+        },
+        {
+            type: "input",
+            message:"",
+            name: "",
+        }
 
+    ]);
+};
 
 // ====================================================================================================
 
+// TODO: call init function @ end of file
 
 
 // TODO: write function to create SVG file called 'logo.svg'
-    // output text in CLI = "Generated logo.svg" after
-    // then 300x200 pixel img that matches choices is shown
+// output text in CLI = "Generated logo.svg" after
+// then 300x200 pixel img that matches choices is shown
 
 
 
